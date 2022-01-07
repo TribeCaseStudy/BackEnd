19d6bab4ce576b5fe7a4fd7d730ea893b75fdede
Deepshikha <deepshikha4862@gmail.com>
2022-01-07 13:10:00 +0530

    empty controller

--- movie-booking-backend/src/main/java/com/ibm/rest/AdminController.java
+++ movie-booking-backend/src/main/java/com/ibm/rest/AdminController.java
@@ -3,6 +3,7 @@ package com.ibm.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import com.ibm.entity.Admin;
 import com.ibm.pojo.Login;
 import com.ibm.service.AdminService;
 
+@CrossOrigin
 @RestController
 public class AdminController {
 	
